2bf61fed0bdb5b3284f9e81c1e3b127781323331
Alef Juan <alefjuan88@gmail.com>
2023-09-23 22:01:00 -0300

    Add TailwindCss como framework da aplicação. Test

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:
+      {
+        main:
+        {
+        '--light': '#f6f6f9',
+        '--amarelo-utfpr': '#FECC00',
+        '--light-primary': '#CFE8FF',
+        '--grey': '#eee',
+        '--dark-grey': '#4e4e4c',
+        '--dark': '#363949',
+        '--vermelho-escuro': '#d84550',
+        '--light-danger': '#FECDD3',
+        '--amarelo': '#FBC02D',
+        '--amarelo-claro': '#FFF2C6',
+        '--verde-escuro': '#5aab35',
+        '--light-success': '#BBF7D0'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
